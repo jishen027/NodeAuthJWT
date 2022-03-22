7e7f00f2e7d5606c6580654e5af8cd60bf5f1af9
JipuLi <jishen027@gmail.com>
2022-03-22 23:29:19 +0000

    [ADD] Cookies Primer

--- node-express-jwt-auth/app.js
+++ node-express-jwt-auth/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRouter = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 
 // view engine
@@ -14,7 +16,7 @@ app.set('view engine', 'ejs');
 
 // database connection
 const dbURI = 'mongodb://localhost:27017/jwt-express';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
@@ -22,3 +24,19 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRouter)
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true')
+  res.cookie('newUser', false)
+  //set cookie for one day
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
+
+  res.send('you got the cookies!')
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies
+  console.log(cookies)
+  res.json(cookies)
+})
