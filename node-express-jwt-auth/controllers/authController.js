970922c7a0e45d7ec9ad01c938c8ae9c059aad9b
JipuLi <jishen027@gmail.com>
2022-03-22 22:21:57 +0000

    [ADD] Mongoose validation

--- node-express-jwt-auth/controllers/authController.js
+++ node-express-jwt-auth/controllers/authController.js
@@ -1,26 +1,47 @@
-const User =  require('../models/User')
+const User = require('../models/User')
 
-module.exports.signup_get = (req,res) => {
+//handle errors
+const handleErrors = (err) => {
+  let errors = { email: '', password: '' }
+
+  //duplicate error code
+  if (err.code == 11000) {
+    errors.email = 'this email is already used'
+    return errors
+  }
+
+  //validation errors
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(error => {
+      errors[error.properties.path] = error.properties.message
+    })
+  }
+
+  return errors
+}
+
+const signup_get = (req, res) => {
   res.render('signup')
 }
 
-module.exports.login_get = (req, res) => {
+const login_get = (req, res) => {
   res.render('login')
 }
 
-module.exports.signup_post = async (req,res) => {
-  console.log(req.body)
-  const {email, password} = req.body
+const signup_post = async (req, res) => {
+  const { email, password } = req.body
 
   try {
-     const user =await User.create({email, password})
-     res.status(201).json(user);
+    const user = await User.create({ email, password })
+    res.status(201).json(user)
   } catch (error) {
-    console.log(error)
-    res.status(400).send('error, user not created')
+    const errors = handleErrors(error)
+    res.status(400).send({ errors })
   }
 }
 
-module.exports.login_post = (req,res) => {
+const login_post = (req, res) => {
   res.send('user login')
 }
+
+module.exports = { signup_get, signup_post, login_get, login_post }
