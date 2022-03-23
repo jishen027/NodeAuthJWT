6748033053adfd31190431b22c6d8d720b6ad37e
JipuLi <jishen027@gmail.com>
2022-03-23 17:17:17 +0000

    [ADD] conditional rendering

--- node-express-jwt-auth/middleware/authMiddleware.js
+++ node-express-jwt-auth/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const User = require('../models/User')
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt
@@ -22,5 +23,28 @@ const requireAuth = (req, res, next) => {
     res.redirect('/login')
   }
 }
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt
 
-module.exports = { requireAuth }
+  if (token) {
+    jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
+      if (err) {
+        //if an error, redirect to login page
+        console.log(err)
+        res.locals.user = null
+        next()
+      } else {
+        console.log(decodedToken)
+        let user = await User.findById(decodedToken.id)
+        res.locals.user = user
+        next()
+      }
+    })
+  } else {
+    res.locals.user = null
+    next()
+  }
+}
+
+module.exports = { requireAuth, checkUser }
